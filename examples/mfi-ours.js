1f0c6870a454218a00b7e464891707d741c94454
Rafal Mi <rafal.m4@gmail.com>
2017-12-11 10:43:29 +0100

    Modified MFI

--- examples/mfi-ours.js
+++ examples/mfi-ours.js
@@ -11,6 +11,11 @@ var marketContents = fs.readFileSync('examples/the.json', 'utf8');
 var marketData = JSON.parse(marketContents);
 
 const MFI = (high, low, close, volume, startIdx, endIdx, optInTimePeriod) => {
+
+    if(startIdx + 1 < optInTimePeriod) {
+        return [];
+    }
+
     let typicalPrice = [];
     let moneyFlow = [];
     let positiveMoneyFlow = [];
@@ -21,32 +26,19 @@ const MFI = (high, low, close, volume, startIdx, endIdx, optInTimePeriod) => {
     let moneyFlowIndex = [];
     
     for(let i = startIdx - optInTimePeriod + 1; i <= endIdx; i++) {
-        if(startIdx + 1 < optInTimePeriod) {
-            return [];
-        }
-
-        typicalPrice[i] = high[i] + low[i] + close[i];
-        moneyFlow[i] = typicalPrice * volume[i];
+        typicalPrice[i] = (high[i] + low[i] + close[i]) / 3;
+        moneyFlow[i] = typicalPrice[i] * volume[i];
         if(i === 0) {
+            positiveMoneyFlow[i] = 0.0;
+            negativeMoneyFlow[i] = 0.0
             continue;
         }
         if(typicalPrice[i] > typicalPrice[i - 1]) {
-            if(i > 0) {
-                positiveMoneyFlow[i] = positiveMoneyFlow[i - 1] + moneyFlow[i];
-                negativeMoneyFlow[i] = 0.0;
-            } else {
-                positiveMoneyFlow[i] = moneyFlow[i];
-                negativeMoneyFlow[i] = 0.0;
-            }
+            positiveMoneyFlow[i] = positiveMoneyFlow[i - 1] + moneyFlow[i];
+            negativeMoneyFlow[i] = 0.0;
         } else {
-            if(i > 0) {
-                positiveMoneyFlow[i] = 0.0;
-                negativeMoneyFlow[i] = negativeMoneyFlow[i - 1] + moneyFlow[i];
-            } else {
-                positiveMoneyFlow[i] = 0.0;
-                negativeMoneyFlow[i] = moneyFlow[i];
-            }
-        
+            positiveMoneyFlow[i] = 0.0;
+            negativeMoneyFlow[i] = negativeMoneyFlow[i - 1] + moneyFlow[i];
         }
         positiveSum += positiveMoneyFlow[i];
         negativeSum += negativeMoneyFlow[i];
@@ -64,9 +56,9 @@ const MFI = (high, low, close, volume, startIdx, endIdx, optInTimePeriod) => {
     return moneyFlowIndex;
 }
 
-let startIdx = 19;
+let startIdx = 3;
 let endIdx = 64;
-let optInTimePeriod = 20;
+let optInTimePeriod = 4;
 
 let moneyFlowIndex = MFI(
     marketData.high,
